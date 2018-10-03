2d6448886483f4f43536f22c9ced33013f1cdfcd
Billy Wu <billy.wu42@gmail.com>
2018-10-03 00:45:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,10 +21,20 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index > 9
+  if position_taken?(board, index) == false
+    return true
+  elsif position_taken?(board, index) == true
     return false
-  elsif index == nil
+  elsif position_taken?(board, index) == nil
     return false
+  end
+end
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    return false
+  elsif board[index] == nil
+    return nil
   else
     return true
   end
